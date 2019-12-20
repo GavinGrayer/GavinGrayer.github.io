0149240442aa6565a57e02eeed0c83e0c78dc9f8
GavinGrayer <236514536@qq.com>
2019-12-20 11:05:28 +0800

    Site updated: 2019-12-20 11:05:24

--- js/runtimeshow.js
+++ js/runtimeshow.js
@@ -1,3 +1,3 @@
-// build time:Fri Dec 20 2019 10:58:50 GMT+0800 (GMT+08:00)
+// build time:Fri Dec 20 2019 11:05:22 GMT+0800 (GMT+08:00)
 var start_date=document.getElementById("runtionshow").getAttribute("start_date");function show_date_time(){BirthDay=new Date(start_date);today=new Date;timeold=today.getTime()-BirthDay.getTime();msPerDay=24*60*60*1e3;e_daysold=timeold/msPerDay;daysold=Math.floor(e_daysold);webinfo_runtime_count.innerHTML=daysold+" "+GLOBAL_CONFIG.runtime_unit}var interval;show_date_time();clearInterval(interval);interval=setInterval(show_date_time,1e4);
 //rebuild by neat 
